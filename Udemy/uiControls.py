405f746a6bb609c6e4ae537df984414d195c0f20
Lee Wing Huen <lhuen2010@gmail.com>
2025-06-27 00:24:23 +0800

    practice

--- Udemy/uiControls.py
+++ Udemy/uiControls.py
@@ -2,4 +2,14 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
-driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+checkboxes = driver.find_elements(By.XPATH, "//input[@type='checkbox']")
+
+print(len(checkboxes))
+
+for checkbox in checkboxes:
+    if checkbox.get_attribute("value") == "option2":
+        checkbox.click()
+        assert checkbox.is_selected()
+        time.sleep(2)
+        break

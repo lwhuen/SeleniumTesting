4b383d62cb51b820c29664f3ec0fb39e08b41459
Lee Wing Huen <lhuen2010@gmail.com>
2025-06-26 23:45:36 +0800

    selenium practice

--- Udemy/locators.py
+++ Udemy/locators.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 driver = webdriver.Chrome()
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 
@@ -13,6 +14,13 @@ driver.find_element(By.ID, "exampleCheck1").click() #click the checkbox
 # CSS: tagname[attribute='value'] -> //input[@type='submit']
 driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Lee")
 driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
+
+#Static Dropdown
+dropdown = Select(driver.find_element(By.ID, "exampleFormControlSelect1"))
+dropdown.select_by_visible_text("Female")
+dropdown.select_by_index(0)
+#dropdown.select_by_value()
+
 driver.find_element(By.XPATH, "//input[@type='submit']").click()
 message = driver.find_element(By.CLASS_NAME, "alert-success").text
 print(message)          # Success! The Form has been submitted successfully!.
